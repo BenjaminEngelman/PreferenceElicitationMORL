045bd5a430502e05bcd63b0db6aa110d631bfa0a
Benjamin Engelman <Benjamin.Engelman@ulb.ac.be>
2020-05-02 19:11:26 +0200

    update

--- src/constants.py
+++ src/constants.py
@@ -36,9 +36,54 @@ BST_DIRECTIONS = ["U", "R", "D", "L"]
 
 # Learning constants
 STEPS_BST = 110_000
+
 STEPS_MINECART_COLD_START = 70_000_000
-# STEPS_MINECART_COLD_START = 10
+N_STEPS_BEFORE_CHECKPOINT = 15_000_000
+STEPS_MINECART_HOT_START = 55_000_000
 
-STEPS_MINECART_HOT_START = 35_000_000
-# STEPS_MINECART_HOT_START = 10
+# Experiments constants
 
+# Each of those weights have another optimal solution for 
+# syntetic PF created by create_3D_pareto_front() in src.ols.utils
+# These are used for the "comparison" experiment
+WEIGHTS_COMP_SYNT = [
+    [0.05, 0.77, 0.18],
+    [0.8, 0.06, 0.14],
+    [0.07, 0.77, 0.16],
+    [0.11, 0.32, 0.57],
+    [0.34, 0.64, 0.02],
+    [0.21, 0.4, 0.39],
+    [0.08, 0.04, 0.88],
+    [0.32, 0.4, 0.28],
+    [0.18, 0.44, 0.38],
+    [0.64, 0.05, 0.31],
+]
+
+# Each of those weights have another optimal solution for 
+# the BST environment
+# These are used for the "comparison" experiment
+WEIGHTS_COMP_BST = [
+    [0.03, 0.97],
+    [0.22, 0.78],
+    [0.23, 0.77],
+    [0.32, 0.68],
+    [0.36, 0.64],
+    [0.49, 0.51],
+    [0.64, 0.36],
+    [0.74, 0.26],
+    [0.8, 0.2],
+    [0.99, 0.01],
+]
+
+# These are used for the "noise" experiment
+WEIGHTS_NOISE_SYNT = [
+    [0.64, 0.05, 0.31],
+    [0.32, 0.4, 0.28],
+    [0.07, 0.77, 0.16],
+]
+
+WEIGHTS_NOISE_BST = [
+    [1.0, 0.0],
+    [0.5, 0.5],
+    [0.1, 0.9]
+]
